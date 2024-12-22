3b0403a914fb4f3cbd2eb35b9d179ac3015c6c96
a1kker <ilya_eliseev_eliseev@list.ru>
2024-12-23 01:09:52 +0300

    Реализовано форматирование данных при выводе в консоль

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -4,31 +4,46 @@
 #include "currency_exchange_rates.h"
 #include "file_reader.h"
 
-using namespace std;
 
 int main()
 {
-    cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #4. Currency exchange rates\n";
-    cout << "Author: Ilya Eliseev\n";
+    std::cout << "Laboratory work #8. GIT\n";
+    std::cout << "Variant #4. Currency exchange rates\n";
+    std::cout << "Author: Ilya Eliseev\n";
 
     try
     {
         std::vector<exchangeRate> rates = FileReader::readFromFile("data.txt");
         for (int i = 0; i < rates.size() - 1; i++)
         {
-            cout << rates[i].bank_name << ' ';
-            cout << rates[i].purchase << ' ';
-            cout << rates[i].sale << ' ';
-            cout << rates[i].bank_adress.city << ' ';
-            cout << rates[i].bank_adress.street << ' ';
-            cout << rates[i].bank_adress.number;
-            cout << '\n';
+            std::cout << "Bank........: ";
+            std::cout << rates[i].bank_name;
+            std::cout << '\n';
+
+            std::cout << "Purchase....: ";
+            std::cout << rates[i].purchase;
+            std::cout << '\n';
+
+            std::cout << "Sale........: ";
+            std::cout << rates[i].sale;
+            std::cout << '\n';
+
+            std::cout << "City........: ";
+            std::cout << rates[i].bank_adress.city;
+            std::cout << '\n';
+
+            std::cout << "Street......: ";
+            std::cout << rates[i].bank_adress.street;
+            std::cout << '\n';
+
+            std::cout << "House number: ";
+            std::cout << rates[i].bank_adress.number;
+            std::cout << '\n';
         }
     }
     catch (const char* error)
     {
-        cout << error << '\n';
+        std::cout << error << '\n';
     }
     return 0;
 }
