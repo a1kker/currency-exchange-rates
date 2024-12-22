880df1bbb9a25802c4a5fb9e7da70bdf1efe3bd1
a1kker <ilya_eliseev_eliseev@list.ru>
2024-12-23 01:16:08 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #4. Currency exchange rates\n";
     cout << "Author: Ilya Eliseev\n";
+    cout << "Group: 24PIng_1D\n";
     return 0;
 }
